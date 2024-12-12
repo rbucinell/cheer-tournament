81e6cb2a617480d83eee5eeb19ccd68306db9880
Ryan Bucinell <rbucinell.dev@gmail.com>
2024-12-12 01:31:35 -0500

    api endpoing to create tournaments

---
+++ src/app/api/tournaments/route.ts
@@ -0,0 +1,24 @@
+import connectDB from '@/app/lib/mongodb';
+import Tournament from '@/app/models/tournament';
+
+export async function GET(request: Request) {
+    return new Response('Hello World', { status: 200 });
+}
+
+export async function POST(request: Request) {
+    await connectDB();
+    const body = await request.json();
+
+    if( !body.name ) return new Response('Name is required', { status: 400 });
+
+    const result = await Tournament.create( new Tournament({
+        name: body.name,
+        description: body.description ?? '',
+        date: body.date ?? new Date(),
+        divisions: body.divisions ?? [ 'Default' ],
+        rubricCategories: body.rubricCategories ?? ['Overall'],
+        judges: body.judges
+    }));
+
+    return new Response(JSON.stringify(result), { status: 200 });
+}
